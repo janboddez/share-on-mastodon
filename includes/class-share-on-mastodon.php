1bdc06a3c33cc5751e99ecf909ca62185426f9a2
Jan Boddez <jan.boddez@gmail.com>
2020-03-15 21:20:11 +0100

    Rename daily cron job

--- includes/class-share-on-mastodon.php
+++ includes/class-share-on-mastodon.php
@@ -21,7 +21,7 @@ class Share_On_Mastodon {
 		register_deactivation_hook( dirname( dirname( __FILE__ ) ) . '/share-on-mastodon.php', array( $this, 'deactivate' ) );
 
 		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
-		add_action( 'share_on_mastodon_daily_cron', array( Options_Handler::get_instance(), 'cron_verify_token' ) );
+		add_action( 'share_on_mastodon_verify_token', array( Options_Handler::get_instance(), 'cron_verify_token' ) );
 
 		$post_handler = Post_Handler::get_instance();
 	}
@@ -34,7 +34,7 @@ class Share_On_Mastodon {
 	public function activate() {
 		// Schedule a daily cron job.
 		if ( false === wp_next_scheduled( 'share_on_mastodon_verify_token' ) ) {
-			wp_schedule_event( time() + DAY_IN_SECONDS, 'daily', 'share_on_mastodon_daily_cron' );
+			wp_schedule_event( time() + DAY_IN_SECONDS, 'daily', 'share_on_mastodon_verify_token' );
 		}
 	}
 
@@ -44,7 +44,7 @@ class Share_On_Mastodon {
 	 * @since 0.4.0
 	 */
 	public function deactivate() {
-		wp_clear_scheduled_hook( 'share_on_mastodon_daily_cron' );
+		wp_clear_scheduled_hook( 'share_on_mastodon_verify_token' );
 	}
 
 	/**
