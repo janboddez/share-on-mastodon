0e65400556b33b66ff1d032ce3919ecc7867d763
Jan Boddez <jan.boddez@gmail.com>
2023-05-07 16:56:51 +0200

    Quick fixes, add custom message field

--- assets/block-editor.js
+++ assets/block-editor.js
@@ -1,25 +1,24 @@
 ( function ( element, components, i18n, data, coreData, plugins, editPost ) {
-	var el                        = element.createElement;
-	var interpolate               = element.createInterpolateElement;
-	var useState                  = element.useState;
-	var CheckboxControl           = components.CheckboxControl;
-	var PanelBody                 = components.PanelBody;
-	var __                        = i18n.__;
-	var sprintf                   = i18n.sprintf;
-	var useSelect                 = data.useSelect;
-	var useEntityProp             = coreData.useEntityProp;
-	var registerPlugin            = plugins.registerPlugin;
-	var PluginSidebar             = editPost.PluginSidebar;
-	var PluginSidebarMoreMenuItem = editPost.PluginSidebarMoreMenuItem;
+	var el                         = element.createElement;
+	var interpolate                = element.createInterpolateElement;
+	var useState                   = element.useState;
+	var TextareaControl            = components.TextareaControl;
+	var ToggleControl              = components.ToggleControl;
+	var __                         = i18n.__;
+	var sprintf                    = i18n.sprintf;
+	var useSelect                  = data.useSelect;
+	var useEntityProp              = coreData.useEntityProp;
+	var registerPlugin             = plugins.registerPlugin;
+	var PluginDocumentSettingPanel = editPost.PluginDocumentSettingPanel;
 
 	function displayUrl( url ) {
 		var parser = new URL( url );
 
 		return sprintf(
-			'<a><b>%1$s</b><c>%2$s</c><d>%3$s</d></a>',
+			'<a><b>%1$s</b><c>%2$s</c><b>%3$s</b></a>',
 			parser.protocol + '://' + ( parser.username ? parser.username + ( parser.password ? ':' + parser.password : '' ) + '@' : '' ),
-			parser.hostname + parser.pathname.slice( 0, 10 ),
-			parser.hostname + parser.pathname.slice( 10 ),
+			parser.hostname.concat( parser.pathname ).slice( 0, 20 ),
+			parser.hostname.concat( parser.pathname ).slice( 20 ),
 		);
 	}
 
@@ -34,83 +33,75 @@
 			controller.abort();
 		}, 6000 );
 
-		window.wp.apiFetch( {
-			path: '/share-on-mastodon/v1/unlink',
-			signal: controller.signal, // That time-out thingy.
-			method: 'POST',
-			data: { post_id: postId },
-		} ).then( function( response ) {
-			clearTimeout( timeoutId );
-		} ).catch( function( error ) {
-			// The request timed out or otherwise failed. Leave as is.
-		} );
+		try {
+			window.wp.apiFetch( {
+				path: '/share-on-mastodon/v1/unlink',
+				signal: controller.signal, // That time-out thingy.
+				method: 'POST',
+				data: { post_id: postId },
+			} ).then( function( response ) {
+				clearTimeout( timeoutId );
+			} ).catch( function( error ) {
+				// The request timed out or otherwise failed. Leave as is.
+				throw new Error( 'The "Unlink" request failed.' )
+			} );
+		} catch ( error ) {
+			return false;
+		}
+
+		return true;
 	}
 
-	registerPlugin( 'share-on-mastodon-sidebar', {
+	registerPlugin( 'share-on-mastodon-panel', {
 		render: function( props ) {
 			var postId   = useSelect( ( select ) => select('core/editor').getCurrentPostId(), [] );
 			var postType = useSelect( ( select ) => select('core/editor').getCurrentPostType(), [] );
 
-			var [ meta, setMeta ] = useEntityProp( 'postType', postType, 'meta' );
-			var isChecked         = false;
+			var [ meta, setMeta ]       = useEntityProp( 'postType', postType, 'meta' );
+			var [ visible, setVisible ] = useState( !! meta._share_on_mastodon_url ); // Using `useState()` to trigger a re-render on change.
 
-			if ( '1' === meta._share_on_mastodon ) {
-				isChecked = true;
-			}
-
-			var [ visible, setVisible ] = useState( !! meta._share_on_mastodon_url );
-
-			return [
-				el( PluginSidebarMoreMenuItem, {
-						target: 'share-on-mastodon-sidebar',
-						icon: 'share'
+			return el( PluginDocumentSettingPanel, {
+					name: 'share-on-mastodon-panel',
+					// icon: 'share',
+					title: __( 'Share on Mastodon', 'share-on-mastodon' ),
+				},
+				el( ToggleControl, {
+					label: __( 'Share on Mastodon', 'share-on-mastodon' ),
+					checked: '1' === meta._share_on_mastodon,
+					onChange: ( newValue ) => {
+						setMeta( { ...meta, _share_on_mastodon: ( newValue ? '1' : '0' ) } );
 					},
-					__( 'Share on Mastodon', 'share-on-mastodon' )
-				),
-				el( PluginSidebar, {
-						name: 'share-on-mastodon-sidebar',
-						icon: 'share',
-						title: __( 'Share on Mastodon', 'share-on-mastodon' ),
+				} ),
+				el( TextareaControl, {
+					label: __( '(Optional) Custom Message', 'share-on-mastodon' ),
+					value: meta._share_on_mastodon_status ? meta._share_on_mastodon_status : '',
+					onChange: ( newValue ) => {
+						setMeta( { ...meta, _share_on_mastodon_status: ( newValue ? newValue : null ) } );
 					},
-					el ( PanelBody, {},
-						el( CheckboxControl, {
-							label: __( 'Share on Mastodon', 'share-on-mastodon' ),
-							checked: isChecked,
-							onChange: ( newValue ) => {
-								setMeta( { ...meta, _share_on_mastodon: ( newValue ? '1' : '0' ) } );
-							},
+				} ),
+				visible
+					? el( 'div', {},
+						// @todo: "Shorten" the URL.
+						interpolate( sprintf( __( 'Shared at %s', 'share-on-mastodon' ), displayUrl( meta._share_on_mastodon_url ) ), {
+							a: el( 'a', { className: 'share-on-mastodon-url', href: encodeURI( meta._share_on_mastodon_url ), target: '_blank', rel: 'noreferrer noopener' } ),
+							b: el( 'span', { className: 'screen-reader-text' } ),
+							c: el( 'span', { className: 'ellipsis' } ),
 						} ),
-						visible
-							? el(
-								'p',
-								{},
-								// @todo: "Shorten" the URL.
-								interpolate( sprintf( __( 'Shared at %s', 'share-on-mastodon' ), displayUrl( meta._share_on_mastodon_url ) ), {
-									a: el( 'a', { className: 'share-on-mastodon-url', href: encodeURI( meta._share_on_mastodon_url ), target: '_blank', rel: 'noreferrer noopener' } ),
-									b: el( 'span', { className: 'screen-reader-text' } ),
-									c: el( 'span', { className: 'ellipsis' } ),
-									d: el( 'span', { className: 'screen-reader-text' } ),
-								} ),
-								el(
-									'a',
-									{
-										className: 'share-on-mastodon-unlink',
-										href: '#',
-										onClick: () => {
-											if ( confirm( __( 'Forget this URL?', 'share-on-mastodon' ) ) ) {
-												unlinkUrl( postId );
-												setVisible( false );
-											}
-										},
-									},
-								__( 'Unlink', 'share-on-mastodon' )
-								)
-							)
-							: null,
-						// @todo: Add that "unlink" button.
+						el( 'a', {
+								className: 'share-on-mastodon-unlink',
+								href: '#',
+								onClick: () => {
+									if ( confirm( __( 'Forget this URL?', 'share-on-mastodon' ) ) ) {
+										unlinkUrl( postId );
+										setVisible( false );
+									}
+								},
+							},
+							__( 'Unlink', 'share-on-mastodon' )
+						)
 					)
-				),
-			];
+					: null,
+			);
 		},
 	} );
 } )( window.wp.element, window.wp.components, window.wp.i18n, window.wp.data, window.wp.coreData, window.wp.plugins, window.wp.editPost );
--- includes/class-blocks.php
+++ includes/class-blocks.php
@@ -55,6 +55,27 @@ class Blocks {
 	 * @todo: (Eventually) also add an "author" endpoint. Or have the single endpoint return both title and author information.
 	 */
 	public static function register_api_endpoints() {
+		$options    = get_options();
+		$post_types = (array) $options['post_types'];
+
+		foreach ( $post_types as $post_type ) {
+			register_post_meta(
+				$post_type,
+				'_share_on_mastodon_status',
+				array(
+					'single'            => true,
+					'show_in_rest'      => true,
+					'type'              => 'string',
+					'auth_callback'     => function() {
+						return current_user_can( 'edit_posts' );
+					},
+					'sanitize_callback' => function( $meta_value ) {
+						return sanitize_textarea_field( $meta_value );
+					},
+				)
+			);
+		}
+
 		register_rest_route(
 			'share-on-mastodon/v1',
 			'/unlink',
@@ -69,10 +90,17 @@ class Blocks {
 	/**
 	 * The one, for now, REST API permission callback.
 	 *
+	 * @param  \WP_REST_Request $request   WP REST API request.
 	 * @return bool If the request's authorized.
 	 */
-	public static function permission_callback() {
-		return current_user_can( 'edit_posts' );
+	public static function permission_callback( $request ) {
+		$post_id = $request->get_param( 'post_id' );
+
+		if ( empty( $post_id ) || ! ctype_digit( $post_id ) ) {
+			return false;
+		}
+
+		return current_user_can( 'edit_post', $post_id );
 	}
 
 	/**
@@ -89,25 +117,18 @@ class Blocks {
 		$post_id = $request->get_param( 'post_id' );
 
 		if ( empty( $post_id ) || ! ctype_digit( $post_id ) ) {
-			// @todo: Make this JSON!
-			status_header( 400 );
-			esc_html_e( 'Missing or incorrect post ID.', 'share-on-mastodon' );
-			wp_die();
+			return new WP_Error( 'invalid_id', 'Invalid post ID.', array( 'status' => 400 ) );
 		}
 
 		$post_id = (int) $post_id;
 
 		if ( ! current_user_can( 'edit_post', $post_id ) ) {
-			// @todo: Make this JSON!
-			status_header( 400 );
-			esc_html_e( 'Insufficient rights.', 'share-on-mastodon' );
-			wp_die();
+			return new WP_Error( 'insufficient_rights', 'Insufficient rights.', array( 'status' => 403 ) );
 		}
 
 		// Have WordPress forget the Mastodon URL.
 		delete_post_meta( $post_id, '_share_on_mastodon_url' );
-		// delete_post_meta( $post_id, '_share_on_mastodon' );
 
-		return new \WP_REST_Response( array( 'status' => 200 ) );
+		return new \WP_REST_Response( array( 'status' => 204 ) );
 	}
 }
