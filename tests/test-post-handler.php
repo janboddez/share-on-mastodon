7cb3daf531a20d931522fc15508db10af8a964c6
Jan Boddez <jan.boddez@gmail.com>
2023-09-18 19:42:33 +0200

    Fix test

--- tests/test-post-handler.php
+++ tests/test-post-handler.php
@@ -10,15 +10,19 @@ class Test_Post_Handler extends \WP_Mock\Tools\TestCase {
 	}
 
 	public function test_post_handler_register() {
-		$post_handler = new \Share_On_Mastodon\Post_Handler();
-
-		\WP_Mock::expectActionAdded( 'transition_post_status', array( $post_handler, 'update_meta' ), 11, 3 );
-		\WP_Mock::expectActionAdded( 'transition_post_status', array( $post_handler, 'toot' ), 999, 3 );
-		\WP_Mock::expectActionAdded( 'share_on_mastodon_post', array( $post_handler, 'post_to_mastodon' ) );
+		$options      = array( 'post_types' => array( 'post', 'page', 'indieblocks_note' ) );
+		$post_handler = new \Share_On_Mastodon\Post_Handler( $options );
 
 		\WP_Mock::expectActionAdded( 'add_meta_boxes', array( $post_handler, 'add_meta_box' ) );
 		\WP_Mock::expectActionAdded( 'admin_enqueue_scripts', array( $post_handler, 'enqueue_scripts' ) );
 		\WP_Mock::expectActionAdded( 'wp_ajax_share_on_mastodon_unlink_url', array( $post_handler, 'unlink_url' ) );
+
+		foreach ( $options['post_types'] as $post_type ) {
+			\WP_Mock::expectActionAdded( "save_post_{$post_type}", array( $post_handler, 'update_meta' ), 10 );
+			\WP_Mock::expectActionAdded( "save_post_{$post_type}", array( $post_handler, 'toot' ), 20 );
+		}
+
+		\WP_Mock::expectActionAdded( 'share_on_mastodon_post', array( $post_handler, 'post_to_mastodon' ) );
 
 		$post_handler->register();
 
