6cbec1978c6057ebff695a41b31fadd713d59feb
Jan Boddez <jan.boddez@gmail.com>
2024-06-22 12:52:27 +0200

    Comment

--- includes/class-syn-links-compat.php
+++ includes/class-syn-links-compat.php
@@ -18,7 +18,7 @@ class Syn_Links_Compat {
 	 */
 	public static function register() {
 		$options    = get_options();
-		$post_types = (array) $options['post_types']; // Should make this more robust.
+		$post_types = (array) $options['post_types'];
 
 		foreach ( $post_types as $post_type ) {
 			add_filter( "get_{$post_type}_syndication_links", array( __CLASS__, 'syndication_links' ), 10, 2 );
